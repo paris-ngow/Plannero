49d3b32b82f75f39730f6a2c78b545fa090059c1
paris-ngow <59807915+paris-ngow@users.noreply.github.com>
2020-11-25 00:16:48 -0500

    Initial Work - Web Application Page Setup
    Created the web application with Java EE environment. Used Apache to setup server. Created and created simple designs of login, register, main, calendar pages.

---
+++ Plannero/src/RegisterHandler.java
@@ -0,0 +1,58 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterHandler
+ */
+@WebServlet("/RegisterHandler")
+public class RegisterHandler extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterHandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//get user submitted information
+			String fname = request.getParameter("fname");
+			String lname = request.getParameter("lname");
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
+				
+			//check if valid in database
+			boolean result = true;
+			if (fname.equals("yes")) {
+				result = true;
+			} else {
+				result = false;
+			}
+					
+			if (result) {
+				response.sendRedirect("jsp/main.jsp");
+			} else {
+				response.sendRedirect("jsp/failedRegister.jsp");
+			}
+	}
+
+}
